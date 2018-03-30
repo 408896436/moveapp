4c3f24456d3029561d13068d957ee80a340d228a
tianxie <408896436@qq.com>
2018-03-30 21:29:10 +0800

    完成了首页的html代码编写,修改了一下nav里a标签的颜色 0.1版本

---
+++ src/template/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    
+    <!--轮播 S-->
+    <div class='lunbo'>
+      <ul>
+        <li><img src="../assets/img1.jpg"></li>
+      </ul>
+    </div>
+    <!--轮播 E-->
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .lunbo img{display: block; width: 100%;}
+  .nav{height: 1.52rem; padding-top: 0.28rem; text-align: center; font-size: 0; -webkit-text-size-adjust:none;}
+  .nav li{ display: inline-block; font-size: 0.24rem; margin:0 0.46rem;}
+  .nav li span{display: block; width: 0.8rem; margin-bottom: 0.2rem;}
+</style>
